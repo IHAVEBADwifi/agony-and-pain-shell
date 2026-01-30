f6f8577619779d868d383c96ec64afef47f02768
IHAVEBADWIFI <hilllam0604@gmail.com>
2026-01-30 14:14:29 +0000

    stuff ig idk

---
+++ backend shennanigans/services/audio.py
@@ -0,0 +1,4 @@
+# recieves signals from UI (Pyside6)
+# uses pipewire, playerctl and cava to make changes (use import os for commands)
+# sends signals back to UI (async)
+# sends signals to event bus (async)
