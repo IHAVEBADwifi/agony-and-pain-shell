36d9231fa9bb3c8ccaebfda601beb7190b41f3f9
IHAVEBADWIFI <hilllam0604@gmail.com>
2026-02-05 18:27:38 +0000

    deleted audio.py and extended plan

--- backend shennanigans/services/audio.py
+++
@@ -1,4 +0,0 @@
-# recieves signals from UI (Pyside6)
-# uses pipewire, playerctl and cava to make changes (use import os for commands)
-# sends signals back to UI (async)
-# sends signals to event bus (async)
